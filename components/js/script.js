fbd0c358086d48991b97b600beff179f78cb6049
Gabriel Felipe de Oliveira Rateiro <gabrielfeliperateirooficial@gmail.com>
2025-01-25 03:19:12 -0300

    Adicionando a feature de Machine Writer, ajustes feitos na responsividade e adiciando comentários para manutenção e compeenção do código no futuro.

--- components/js/script.js
+++ components/js/script.js
@@ -1,8 +1,33 @@
 const navbar = document.querySelector(".navbar");
 const btnMobile = document.querySelector(".fa-bars");
+const title = document.querySelector("h1");
 
 btnMobile.addEventListener('click', () => {
 
     navbar.classList.toggle('active');
 
 })
+
+function machineWriter(element) {
+
+    
+    setInterval(() => {
+
+        const textArray = element.innerHTML.split('');
+        element.innerHTML = "";
+        
+        textArray.forEach((letter, i) => { /* letter = letra and i = index */
+        
+            setTimeout(() => {
+    
+                element.innerHTML += letter;
+    
+            }, 160 * i)
+    
+        });
+
+    }, 5800);
+
+}
+
+machineWriter(title);
